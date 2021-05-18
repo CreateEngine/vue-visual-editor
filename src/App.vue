2bcd85d98dbf463fb28718f74eff055cb84e43ea
shifeng <312265535@qq.com>
2021-05-18 22:15:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <h1> vue-visual-editor</h1>
+    <p>采用vue2.0+element+vant可拖拽的可视化编辑器</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
