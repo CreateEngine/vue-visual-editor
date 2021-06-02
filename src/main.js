002ff9df509ee4436a0a07e1b48ec79bbba48c3c
shifeng <312265535@qq.com>
2021-06-02 18:04:07 +0800

    依赖

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
+import store from './store';
+import router from './router';
 
 Vue.use(ElementUI);
 Vue.use(Antd);
@@ -11,5 +13,7 @@ Vue.use(Antd);
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
